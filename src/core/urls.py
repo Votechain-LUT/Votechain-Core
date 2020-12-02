179ce00720d71280582effdbc04f16e2e006f335
Adam Troszczyński <37688309+210342@users.noreply.github.com>
2020-12-02 18:44:45 +0100

    Added docs in admin panel (#2)

--- src/core/urls.py
+++ src/core/urls.py
@@ -13,14 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.views.defaults import page_not_found
 from django.urls import path, include
-from core.views import admin_view
+from core.views import admin_poll_view
+
+from core.admin import admin_view
+admin.site.admin_view = admin_view
 
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
+    path('admin/login/', page_not_found, kwargs={'exception': Exception('Page not Found')}),
+    path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('poll/<int:pk>/', admin_view.AdminPoll.as_view(), name='admin_poll'),
-    path('poll/create/', admin_view.AdminCreatePoll.as_view(), name='admin_create_poll'),
-    path('poll/start/', admin_view.AdminStartPoll.as_view(), name='admin_start_poll'),
-    path('poll/list/', admin_view.AdminListPoll.as_view(), name='admin_list_poll'),
+    path('poll/<int:pk>/', admin_poll_view.AdminPoll.as_view(), name='admin_poll'),
+    path('poll/create/', admin_poll_view.AdminCreatePoll.as_view(), name='admin_create_poll'),
+    path('poll/start/', admin_poll_view.AdminStartPoll.as_view(), name='admin_start_poll'),
+    path('poll/list/', admin_poll_view.AdminListPoll.as_view(), name='admin_list_poll'),
 ]
