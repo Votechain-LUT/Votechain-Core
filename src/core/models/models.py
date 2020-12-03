afb0f29bfa75965ff4aebff5bea63afe1427f25a
Jakub Garstka <jakub.garstka97@gmail.com>
2020-12-03 23:55:51 +0100

    Code review update ;)

--- src/core/models/models.py
+++ src/core/models/models.py
@@ -7,17 +7,18 @@ class Poll(models.Model):
     created = models.DateTimeField(auto_now=True)
     start = models.DateTimeField(blank=True, db_index=True, null=True)
     end = models.DateTimeField(blank=False)
+    isActive = models.BooleanField(blank=False)
 
 class Candidate(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=500)
-    poll = models.ForeignKey (Poll, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     
 class Candidate_result():
 
-    def __init__(self, numberOfVotes, candidate):
-        self.numberOfVotes:int = numberOfVotes
-        self.candidate:Candidate = candidate
+    def __init__(self, numberOfVotes:int, candidate: Candidate):
+        self.numberOfVotes = numberOfVotes
+        self.candidate = candidate
     
 class Vote(models.Model):
     id = models.BigAutoField(primary_key=True)
