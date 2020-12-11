35ebb6c1ab8eff614d2bc908b1409fb071b7909b
210342 <210342@edu.p.lodz.pl>
2020-12-11 20:02:25 +0100

    The static files

--- src/votechain/urls.py
+++ src/votechain/urls.py
@@ -14,7 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.conf.urls.static import static
+from votechain import settings
 
 urlpatterns = [
     path('', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
