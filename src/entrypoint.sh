7039516fe177deae550c005e4af1d15077ff2454
210342 <210342@edu.p.lodz.pl>
2020-12-15 17:29:03 +0100

    Fixtures

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -9,7 +9,7 @@ while [[ $STATUS -ne 52 ]] && [[ $i -lt 60 ]]
 do
 	let i=i+1
     sleep 1s
-	curl $DATABASE_HOST:1433
+	curl $DATABASE_HOST:$DATABASE_PORT
 	STATUS=$?
 done
 
@@ -21,6 +21,8 @@ echo "CREATE SUPERUSER"
 python3 manage.py createsuperuser --noinput
 echo "COLLECT STATIC"
 python3 manage.py collectstatic --noinput --clear
+echo "SEED DATA"
+python3 manage.py loaddata --app core core/fixtures/dev_data.json
 
 echo "STARTING UP HTTP LISTENER"
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:$PORT
