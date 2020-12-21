6d41490d5d34de05c7fa6ae2fe2dedeb72fe7543
Adam Troszczyński <37688309+210342@users.noreply.github.com>
2020-12-21 08:24:21 +0100

    Added default start value (#25)

--- src/core/models/models.py
+++ src/core/models/models.py
@@ -15,7 +15,7 @@ class Poll(models.Model):
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=256, blank=False)
     created = models.DateTimeField(auto_now_add=True)
-    start = models.DateTimeField(blank=True, db_index=True, null=True)
+    start = models.DateTimeField(blank=True, db_index=True, null=True, default=timezone.now)
     end = models.DateTimeField(blank=False)
     isActive = models.BooleanField(blank=False, default=False)
 
