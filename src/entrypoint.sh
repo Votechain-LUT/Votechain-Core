cb29d118c16e36a412cf6f3a8319feb8bb13454a
210342 <210342@edu.p.lodz.pl>
2021-01-13 18:03:22 +0100

    build

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,7 +3,9 @@
 /bin/sh ./await.sh
 
 echo "COLLECT STATIC"
-python3 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clearecho
+"SEED DATA"
+python3 manage.py loaddata --app core core/fixtures/dev_data.json
 
 echo "STARTING UP HTTP LISTENER"
 python manage.py runserver 0.0.0.0:$PORT
